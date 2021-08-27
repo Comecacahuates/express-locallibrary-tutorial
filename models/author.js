d84de2c3683011fb905345f744fc3860fffb875d
Adrián Juárez <comecacahuates@yahoo.com>
2021-08-27 04:51:08 -0500

    Modelos

---
+++ models/author.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxlength: 100 },
+  family_name: { type: String, required: true, maxlength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtual for author's lifespan
+AuthorSchema.virtual("lifespan").get(() => {
+  let lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocalString(
+      DateTime.DATE_MED
+    );
+  }
+  lifetime_string += " - ";
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocalString(
+      DateTime.DATE_MED
+    );
+  }
+  return lifetime_string;
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual("url").get(() => `/catalog/author/${this._id}`);
+
+// Export model
+module.exports = mongoose.model("Author", AuthorSchema);
