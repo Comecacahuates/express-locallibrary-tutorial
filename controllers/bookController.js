a615b68008b27247554332fc67fac618f08d9249
Adrián Juárez <comecacahuates@yahoo.com>
2021-08-28 04:49:47 -0500

    Página de inicio

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,8 +1,37 @@
-const { response } = require("express");
 const Book = require("../models/book");
+const Author = require("../models/author");
+const Genre = require("../models/genre");
+const BookInstance = require("../models/bookinstance");
+
+const async = require("async");
 
 exports.index = (request, response) => {
-  response.send("NOT IMPLEMENTED: Site Home Page");
+  async.parallel(
+    {
+      book_count: (callback) => {
+        Book.countDocuments({}, callback);
+      },
+      bookinstance_count: (callback) => {
+        BookInstance.countDocuments({}, callback);
+      },
+      bookinstance_availabe_count: (callback) => {
+        BookInstance.countDocuments({ status: "Available" }, callback);
+      },
+      author_count: (callback) => {
+        Author.countDocuments({}, callback);
+      },
+      genre_count: (callback) => {
+        Genre.countDocuments({}, callback);
+      },
+    },
+    (error, results) => {
+      response.render("index", {
+        title: "Local Libreary Home",
+        error: error,
+        data: results,
+      });
+    }
+  );
 };
 
 /**
